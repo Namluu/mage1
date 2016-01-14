ee9c40d78d76e32c2649510aaefc2f4eb23375fc
Nam Luu <nam.luuduc@gmail.com>
2016-01-14 14:01:29 +0700

    add helloworld module

---
+++ app/code/local/Tomluu/Helloworld/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Tomluu_Helloworld_IndexController extends Mage_Core_Controller_Front_Action 
+{
+    public function indexAction()
+    {
+        echo 'Helloworld';
+        //$this->loadLayout();
+        //$this->renderLayout();
+    }
+}
